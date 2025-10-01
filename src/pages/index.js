47bee03ec781114d3c94d2b6b8734c2b15438235
Sunnykrp <sunnykrp3@gmail.com>
2025-10-02 01:16:42 +0530

    setup project folder structure

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <h1 className="text-4xl font-bold">hello</h1>
+    </div>
+  );
+}
